5dfab42df63df3dec9a068fffc59e9736f8a55df
Doroshchuk <ddv.doroshchuk@gmail.com>
2025-09-13 18:39:57 -0700

    - Updated .gitignore to specify resume file paths. - Refactored main.py to improve resume parsing logic. - Enhanced prompt instructions in prompt_instructions.json for clarity on keyword integration. - Updated whitelisted hard/soft skills within settings.json.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 from core.parsing.parser import ResumeParser
 from core.services.config_manager import ConfigManager
 from core.jobscan.scraper import JobscanScraper
-from core.utils.helpers import JobParserUtils
+from core.utils.helpers import JobParserUtils, MatchReportParserUtils, ResumeParserUtils
 import core.utils.paths as path_utils
 from core.services.cv_tailor import TailorAIService
+from pathlib import Path
 
 
 config = ConfigManager()
-resume_parser = ResumeParser(path_utils.get_original_resume_file_path())
-resume = resume_parser.parse()
-resume.write_to_file()
+if not path_utils.get_parsed_resume_file_path().is_file():
+    resume_parser = ResumeParser(path_utils.get_original_resume_file_path())
+    resume = resume_parser.parse()
+    resume.write_to_file()
+else:
+    resume = ResumeParserUtils.parse_resume(path_utils.get_parsed_resume_file_path())
 job_details = JobParserUtils.parse_job_details(path_utils.get_job_to_target_file_path())
 jobscan_scraper = JobscanScraper(config.settings.jobscan, config.settings.playwright, config.settings.resume, job_details)
 match_report = jobscan_scraper.run_resume_scan_workflow()
 tailor_ai_service = TailorAIService(job_details)
 tailored_resume = tailor_ai_service.tailor_cv(resume, match_report.get_keywords_to_prompt())
+tailored_resume.write_to_file(job_details.company, job_details.title)

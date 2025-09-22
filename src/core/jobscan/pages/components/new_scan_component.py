17c528f750cfbbbeecad38c8eeddae747c1db6ac
Doroshchuk <ddv.doroshchuk@gmail.com>
2025-09-21 21:32:56 -0700

    Refactored DashboardPage to delegate scan functionality to NewScanComponent, improving clarity and code reuse
    - Introduced NewScanComponent to encapsulate resume upload and scanning logic.
    - Updated DashboardPage to delegate scanning operations to NewScanComponent, enhancing code organization and readability.
    - Removed redundant methods from DashboardPage, streamlining the overall structure.

---
+++ src/core/jobscan/pages/components/new_scan_component.py
@@ -0,0 +1,44 @@
+from playwright.sync_api import Locator, Page, expect
+from core.utils.ui_helpers import PlaywrightHelper
+from core.models.settings import ResumeSettings
+from core.models.job_to_target import JobDetails
+
+
+class NewScanComponent:
+    def __init__(self, container: Locator, page: Page, playwright_helper: PlaywrightHelper, resume_settings: ResumeSettings) -> None:
+        self.container = container
+        self.page = page
+        self.playwright_helper = playwright_helper
+        self.resume_settings = resume_settings
+
+    @property
+    def resume_text_area(self) -> Locator:
+        return self.container.get_by_placeholder("Paste resume text...")
+
+    @property
+    def resume_drag_and_drop_button(self) -> Locator:
+        return self.container.locator("div.resumeActions > label.upload")
+
+    @property
+    def resume_upload_input(self) -> Locator:
+        return self.container.locator("div.inputArea input#file")
+
+    @property
+    def job_description_text_area(self) -> Locator:
+        return self.container.locator("textarea#jobDescriptionInput")
+
+    @property
+    def scan_button(self) -> Locator:
+        return self.container.locator("//span[normalize-space(.) = 'Scan']/parent::button")
+
+    def upload_resume(self, path_to_resume: str) -> None:
+        self.playwright_helper.human_like_mouse_move_and_click(self.page, self.resume_text_area)
+        with self.page.expect_file_chooser() as fch:
+            self.playwright_helper.delayed_hover_and_click(self.resume_drag_and_drop_button)
+            fch.value.set_files(path_to_resume)
+
+    def scan(self, path_to_resume: str, job_details: JobDetails) -> None:
+        self.upload_resume(path_to_resume)
+        self.playwright_helper.human_like_fill_data(self.page, self.job_description_text_area,  str(job_details))
+        expect(self.scan_button).to_be_enabled(timeout=2000)
+        self.playwright_helper.human_like_mouse_move_and_click(self.page, self.scan_button)
--- src/core/jobscan/pages/dashboard_page.py
+++ src/core/jobscan/pages/dashboard_page.py
@@ -3,6 +3,7 @@ from core.utils.ui_helpers import PlaywrightHelper
 from core.jobscan.pages.match_report_page import MatchReportPage
 from core.models.settings import JobscanSettings, ResumeSettings
 from core.models.job_to_target import JobDetails
+from core.jobscan.pages.components.new_scan_component import NewScanComponent
 
 
 class DashboardPage:
@@ -11,23 +12,13 @@ class DashboardPage:
         self.jobscan_settings = jobscan_settings
         self.playwright_helper = playwright_helper
         self.resume_settings = resume_settings
-
-        self.resume_text_area = self.page.get_by_placeholder("Paste resume text...")
-        self.resume_drag_and_drop_button = self.page.locator("div.resumeActions > label.upload")
-        self.resume_upload_input = self.page.locator("div#scanUploader div.inputArea input#file")
-        self.job_description_text_area = self.page.locator("textarea#jobDescriptionInput")
-        self.scan_button = self.page.locator("//span[normalize-space(.) = 'Scan']/parent::button")
-
-    def upload_resume(self, path_to_resume: str) -> None:
-        self.playwright_helper.human_like_mouse_move_and_click(self.page, self.resume_text_area)
-        with self.page.expect_file_chooser() as fch:
-            self.playwright_helper.delayed_hover_and_click(self.resume_drag_and_drop_button)
-            fch.value.set_files(path_to_resume)
+        self.new_scan_component = NewScanComponent(
+            container=self.page.locator("div#scanUploader"),
+            page=self.page,
+            playwright_helper=self.playwright_helper,
+            resume_settings=self.resume_settings)
 
     def scan(self, path_to_resume: str, job_details: JobDetails) -> MatchReportPage:
-        self.upload_resume(path_to_resume)
-        self.playwright_helper.human_like_fill_data(self.page, self.job_description_text_area,  str(job_details))
-        expect(self.scan_button).to_be_enabled(timeout=2000)
-        self.playwright_helper.human_like_mouse_move_and_click(self.page, self.scan_button)
+        self.new_scan_component.scan(path_to_resume, job_details)
         self.page.wait_for_url(self.jobscan_settings.match_report_url_pattern, timeout=15000)
-        return MatchReportPage(self.page, self.playwright_helper, self.resume_settings, job_details)
+        return MatchReportPage(page=self.page, playwright_helper=self.playwright_helper, jobscan_settings=self.jobscan_settings, resume_settings=self.resume_settings, job_details=job_details)
